b7a52dfc2ecba645a52628f43059e6eda73f62da
Wang Qun Feng <qunfengw@gmail.com>
2017-10-15 10:50:39 +0800

    move the CSS into its own file

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -81,7 +81,7 @@ class SearchBooks extends Component {
                     <li key={book.id}>
                         <div className="book">
                         <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-cover" style={book.imageLinks.thumbnail && {backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                             <div className="book-shelf-changer">
                             <select value={book.shelf? book.shelf: "none"} onChange={(event) =>{
                                 this.updateShelf(book, event.target.value)
